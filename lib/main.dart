5fc43f33bad845adf62ac375aa3f4fef9d718ed5
Tanishq Chugh <tanishqchugh85@gmail.com>
2021-12-19 21:46:44 +0530

    A type of BusinessCard using Flutter
    Learning UI using flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,98 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/image.png')),
+              Text(
+                'Tanishq Chugh',
+                style: TextStyle(
+                  fontSize: 30.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                ),
+              ),
+              Text(
+                'FLUTTER PROGRAMMER',
+                style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                    fontFamily: 'Source',
+                    letterSpacing: 2.5),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(70, 0, 70, 0),
+                child: SizedBox(
+
+                  height: 20,
+                  child: Divider(color: Colors.black,
+                  height: 5,
+                  thickness: 1,
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.only(left: 50, right: 50),
+                // padding: EdgeInsets.all(10),
+
+                child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '+91 9205232357',
+                      style: TextStyle(
+                          color: Colors.teal,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          wordSpacing: 2),
+                    )),
+              ),
+              Container(
+                height: 10,
+                color: Colors.teal,
+              ),
+              Card(
+                margin: EdgeInsets.only(left: 40, right: 40),
+                // padding: EdgeInsets.all(10),
+
+                child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    // SizedBox(
+                    //   width: 10,
+                    // ),
+                    title: Text(
+                      'tanishqchugh85@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        // wordSpacing: 2
+                      ),
+                    )),
+              )
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
